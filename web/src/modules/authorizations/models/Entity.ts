5728458878283701dc3adf19441fa0a7d966bb02
Mathieu Collinge <shabla@gmail.com>
2021-07-22 02:21:16 -0400

    new authorizations module wip

---
+++ web/src/modules/authorizations/models/Entity.ts
@@ -0,0 +1,8 @@
+export interface Entity {
+    description?: string;
+    metadata?: Record<string, unknown>;
+    created_at?: string;
+    created_by?: string;
+    updated_at?: string;
+    updated_by?: string;
+}
